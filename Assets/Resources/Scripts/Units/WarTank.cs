acef28cf9dc3189aa969a9e9b30bd697d9044cfa
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-08-07 15:01:47 +0200

    Fixed drag issue

--- Assets/Resources/Scripts/Units/WarTank.cs
+++ Assets/Resources/Scripts/Units/WarTank.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WarTank : MonoBehaviour {
 
-  public CameraControl m_Camera;
+  private CameraControl m_Camera;
 
   // Use this for initialization
   void Start() {
