679de9c93e8c6faec68fa0d7d1b00e4eb7740a29
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-08-09 17:54:33 +0200

    Added a pause menu to the game

---
+++ Assets/Resources/Scripts/Managers/PauseMenuManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour {
+
+  public static PauseMenuManager instance;
+  public GameObject m_PauseMenu;
+
+  // Use this for initialization
+  void Awake() {
+    if(instance == null) {
+      instance = this;
+    } else if (instance != this) {
+      Destroy(gameObject);
+    }
+  }
+
+
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      if (m_PauseMenu.activeSelf) {
+        UnPause();
+      } else {
+        Pause();
+      }
+    }
+
+    if (m_PauseMenu.activeSelf && Time.timeScale != 0f) {
+      Time.timeScale = 0f;
+    }
+  }
+
+
+  public void Pause() {
+    m_PauseMenu.SetActive(true);
+    Time.timeScale = 0f;
+  }
+
+
+  public void UnPause() {
+    m_PauseMenu.SetActive(false);
+    Time.timeScale = 1f;
+  }
+
+
+  public void BackToMainMenu() {
+    SceneManager.LoadScene("MainMenu");
+  }
+
+
+  public void Quit() {
+    Application.Quit();
+  }
+}
