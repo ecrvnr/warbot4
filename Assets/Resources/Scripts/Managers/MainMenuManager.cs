f95fcac52ff9cbe42740f2e8b9d0f8ffc34e184a
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-08-09 17:10:26 +0200

    Added Options menu and linked menus together

---
+++ Assets/Resources/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+  public static MainMenuManager instance;
+
+
+  void Awake() {
+    if (instance == null) {
+      instance = this;
+    } else if (instance != this) {
+      Destroy(gameObject);
+    }
+  }
+
+
+  public void EnableMenu(GameObject menu) {
+    menu.SetActive(true);
+  }
+
+
+  public void DisableMenu(GameObject menu) {
+    menu.SetActive(false);
+  }
+
+
+  public void StartGame() {
+    SceneManager.LoadScene("Game");
+  }
+
+
+  public void Quit() {
+    Application.Quit();
+  }
+}
