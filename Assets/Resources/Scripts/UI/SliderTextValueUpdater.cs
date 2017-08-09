6445c8e4159f0918a4d6f5cf8d43eec8b85601a8
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-08-10 00:51:06 +0200

    Enabled sliders and made map selection toggle group work as intended

---
+++ Assets/Resources/Scripts/UI/SliderTextValueUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderTextValueUpdater : MonoBehaviour {
+
+  private Slider m_SliderSelf;
+  private Text m_ValueText;
+
+  // Use this for initialization
+  void Start() {
+    m_SliderSelf = GetComponent<Slider>();
+    m_ValueText = transform.Find("ValueText").GetComponent<Text>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (m_SliderSelf.wholeNumbers) {
+      m_ValueText.text = m_SliderSelf.value.ToString();
+    } else {
+      m_ValueText.text = System.Math.Round(m_SliderSelf.value , 2).ToString();
+    }
+  }
+}
