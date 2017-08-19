5bbe9221621660b73c8ec79cb97ad493e29396c1
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-08-19 14:19:02 +0200

    Added Action class

---
+++ Assets/Resources/Scripts/Subsumption/Action.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Action {
+
+  public string m_DoingActionFeedback;
+  public string m_ActionDoneFeedback;
+  public bool m_ActionDone;
+
+
+  public Action() {
+    m_ActionDone = false;
+  }
+
+  public abstract void DoAction();
+}
