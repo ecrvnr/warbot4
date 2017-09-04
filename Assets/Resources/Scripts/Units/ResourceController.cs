9a6e26af4df9dbeb1e45a03822f4d8e1afbb37dd
nicolas.stadler <nicolas.stadler@etu.umontpellier.fr>
2017-09-04 18:12:29 +0200

    Wrote basic resource controller

---
+++ Assets/Resources/Scripts/Units/ResourceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceController : MonoBehaviour {
+
+  public float m_MaxResources;
+  [HideInInspector] public float m_CurrentResources;
+
+  private WarBot m_WarBot;
+
+  void Awake() {
+    m_CurrentResources = m_MaxResources;
+  }
+
+  // Use this for initialization
+  void Start() {
+    m_WarBot = gameObject.GetComponent<WarBot>();
+  }
+}
